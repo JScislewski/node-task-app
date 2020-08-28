fcd63b3fc0fe08285e6cee8349c8bd7b6e36752b
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-08-28 23:09:18 +0200

    Add validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
@@ -9,21 +10,47 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 const User = mongoose.model("User", {
   name: {
     type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid!");
+      }
+    },
+  },
+  password: {
+    type: String,
+    require: true,
+    trim: true,
+    minLength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error('Password cannot contain "password"');
+      }
+    },
   },
   age: {
     type: Number,
+    validate(value) {
+      if (value < 0 || value > 100) {
+        throw new Error("Age must be a positive number lesser than 100");
+      }
+    },
   },
 });
 
-const me = new User({
-  name: "Kuba",
-  age: 24,
+const Task = mongoose.model("Task", {
+  description: {
+    type: String,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
-
-me.save()
-  .then(() => {
-    console.log(me);
-  })
-  .catch((error) => {
-    console.log("ERROR: ", error);
-  });
