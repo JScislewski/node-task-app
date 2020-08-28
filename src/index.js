6b2ce39c9a56d4d650e291f0453f207ba0374337
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-08-28 23:15:52 +0200

    Add express

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-const connection = require("./db/mongoose");
+const express = require("express");
 
-connection;
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server is running on port:", port);
+});
