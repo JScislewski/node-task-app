438790f94c5b24ae6d77bc6d1463fffc3d177100
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-08-29 15:59:53 +0200

    Edit task updating endpoint

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const Task = require("../models/task");
+const { update } = require("../models/task");
 const router = new express.Router();
 
 router.get("/tasks", async (req, res) => {
@@ -53,10 +54,8 @@ router.patch("/tasks/:id", async (req, res) => {
   }
 
   try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const task = await Task.findById(req.params.id);
+    updates.forEach((update) => (task[update] = req.body[update]));
 
     if (!task) {
       return res.status(404).send();
