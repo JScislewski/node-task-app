f298d0dbe919203ba2138db596fd7b012aac0470
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-08-29 03:34:51 +0200

    Add endpoint for deleting task

--- src/index.js
+++ src/index.js
@@ -95,6 +95,18 @@ app.get("/tasks/:id", async (req, res) => {
   }
 });
 
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 app.patch("/tasks/:id", async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["description", "completed"];
