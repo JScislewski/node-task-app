a7da7141ed12bdb3c6d81d2e62d74f3b99793bd8
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-08-29 22:46:59 +0200

    Add test for creating user

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,11 +5,13 @@ const router = new express.Router();
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
+
   try {
     await user.save();
-    res.status(201).send(user);
+
+    res.status(201).send({ user });
   } catch (e) {
-    res.status(500).send(e);
+    res.status(400).send(e);
   }
 });
 
